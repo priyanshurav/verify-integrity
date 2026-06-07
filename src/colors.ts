71ea35abdcadfed7ff9dfc4c54dea672634d158b
Priyanshu Rav <77456300+priyanshurav@users.noreply.github.com>
2026-06-07 19:41:34 +0530

    chore(colors): rename colours to colors

--- src/colors.ts
+++ src/colors.ts
@@ -1,10 +1,10 @@
-import type { Colour } from './types.js';
+import type { Color } from './types.js';
 
-export const createColours = (noColour: boolean) => {
+export const createColors = (noColor: boolean) => {
   const format = (openCode: string, str: string): string => {
-    return !noColour ? `\x1b[${openCode}m${str}\x1b[0m` : str;
+    return !noColor ? `\x1b[${openCode}m${str}\x1b[0m` : str;
   };
-  const colours = {
+  const colors = {
     yellow: (message) => format('33', message),
     red: (message) => format('91', message),
     redBold: (message) => format('1;91', message),
@@ -12,7 +12,7 @@ export const createColours = (noColour: boolean) => {
     greenBold: (message) => format('1;92', message),
     cyan: (message) => format('36', message),
     cyanBold: (message) => format('1;36', message),
-  } satisfies Record<string, Colour>;
+  } satisfies Record<string, Color>;
 
-  return colours;
+  return colors;
 };
